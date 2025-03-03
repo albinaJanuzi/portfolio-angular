82662f690ae9c47446cc5c59bc694a4cf1d71c77
Albina Januzi <albina_januzi@hotmail.com>
2025-03-03 16:21:27 +0200

    Legal Notice Navbar, Translate

--- src/app/legal-notice/legal-notice.component.ts
+++ src/app/legal-notice/legal-notice.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FooterComponent } from '../main-content/footer/footer.component';
+import { NavbarComponent } from '../main-content/landing-page/navbar/navbar.component';
+import { TranslateDirective, TranslatePipe, TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-legal-notice',
-  imports: [RouterModule, FooterComponent],
+  imports: [RouterModule, FooterComponent,TranslatePipe,TranslateDirective,NavbarComponent],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss'
 })
 export class LegalNoticeComponent {
-
+  constructor(private translate: TranslateService) {}
+  changeLanguage(language: string){
+    this.translate.use(language);
+  }
 }
