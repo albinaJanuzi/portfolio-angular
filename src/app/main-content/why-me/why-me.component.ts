95a1cbbf8e4d09df380dae3ad89d65bac389c500
Albina Januzi <albina_januzi@hotmail.com>
2025-04-23 14:30:14 +0200

    Text Modify & keyframes in why me section

--- src/app/main-content/why-me/why-me.component.ts
+++ src/app/main-content/why-me/why-me.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import {TranslatePipe, TranslateDirective} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-why-me',
-  imports: [TranslatePipe, TranslateDirective],
+  imports: [TranslatePipe, TranslateDirective, CommonModule],
   templateUrl: './why-me.component.html',
   styleUrl: './why-me.component.scss'
 })
